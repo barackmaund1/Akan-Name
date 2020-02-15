4c9ceb324b9cbbea60da6e7da6de4d5b5396959f
barackmaund1 <pmaundu49@gmail.co>
2020-02-15 12:23:43 +0300

    add and dalete channges

--- day.js
+++ day.js
@@ -1,13 +1,13 @@
 function myFunction() {
   var day = parseInt( document.getElementById("day").value);
   var d = new Date();
-  var DD = d.getDate(day);
+  var DD =d.getDate(day);
   var month =parseInt( document.getElementById("month").value);
-  var MM = d.getMonth(month) + 1;
+  var MM =d.getMonth(month);
   var year =parseInt( document.getElementById("year").value);
-  var YY = d.getFullYear(year);
+  var YY =d.getFullYear(year);
   var CC = (YY - 1) / 100 + 1;
-  var dayOfWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+  var dayOfWeek = parseInt((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
   var male = document.getElementById('male');
   var female = document.getElementById("female");
   var weekDay=Math.ceil(dayOfWeek);
@@ -19,7 +19,7 @@ function myFunction() {
     alert("Invalid data for the month!! please enter valid month");
   }
   // validating year
-  else if (year <= 1699 || year >= 2020) {
+  else if (year <= 1699 || year > 2020) {
     alert("Invalid year!!! Please enter a valid year");
   }
   //   else if (yy.toString().length !== 4) {
